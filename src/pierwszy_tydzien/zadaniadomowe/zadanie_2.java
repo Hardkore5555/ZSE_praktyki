35126a45a40731dee290512b4c54e291577c919d
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-25 13:02:55 +0100

    dodano sum

--- src/pierwszy_tydzien/zadaniadomowe/zadanie_2.java
+++ src/pierwszy_tydzien/zadaniadomowe/zadanie_2.java
@@ -4,6 +4,8 @@ public class zadanie_2 {
     public static void main(String[] args) {
         int A = 2;
         int B = 3;
+        int sum = (A*A)+(B*B);
+        System.out.println("wynik = "+ sum) ;
     }
 
 }
