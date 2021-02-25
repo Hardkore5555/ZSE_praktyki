062496231fe247fb64595740b94637cdd3ade0f1
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-25 12:13:31 +0100

    zadeklarowano zmienne

--- src/pierwszy_tydzien/basics/lekcja_podstawy.java
+++ src/pierwszy_tydzien/basics/lekcja_podstawy.java
@@ -3,7 +3,11 @@ package pierwszy_tydzien.basics;
 public class lekcja_podstawy {
 
     public static void main(String[] args) {
-        
+
+        int A = 1;
+        int B = -32;
+        int C =  77;
+
     }
 
 
