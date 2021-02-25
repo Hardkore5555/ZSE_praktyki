401ea193b7d89b8caeb84fd6a5cc2bf2e2855170
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-25 12:14:26 +0100

    dodano komentarz dla psvm

--- src/pierwszy_tydzien/basics/lekcja_podstawy.java
+++ src/pierwszy_tydzien/basics/lekcja_podstawy.java
@@ -1,14 +1,17 @@
 package pierwszy_tydzien.basics;
 
 public class lekcja_podstawy {
-
+// to jest metoda główna
     public static void main(String[] args) {
 
         int A = 1;
         int B = -32;
         int C =  77;
 
-    }
 
+
+
+
+    }
 
 }
