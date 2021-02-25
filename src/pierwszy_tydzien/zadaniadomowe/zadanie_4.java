578fede376e8bad56a6249facfcb0bcdaefd8a8f
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-25 13:55:40 +0100

    ukończono zadanie 4

--- src/pierwszy_tydzien/zadaniadomowe/zadanie_4.java
+++ src/pierwszy_tydzien/zadaniadomowe/zadanie_4.java
@@ -2,6 +2,9 @@ package pierwszy_tydzien.zadaniadomowe;
 
 public class zadanie_4 {
     public static void main(String[] args) {
-        
+        String imie = "Krystian";
+        String nazwisko = "Lewandowski";
+        System.out.println("Imię: "+imie);
+        System.out.println("Nazwisko: "+nazwisko);
     }
 }
