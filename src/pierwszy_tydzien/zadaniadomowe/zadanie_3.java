b631e95a9bbd7f5c65f13faacdce64af44800f93
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-25 13:51:16 +0100

    zakończono zadanie3

--- src/pierwszy_tydzien/zadaniadomowe/zadanie_3.java
+++ src/pierwszy_tydzien/zadaniadomowe/zadanie_3.java
@@ -3,8 +3,7 @@ package pierwszy_tydzien.zadaniadomowe;
 public class zadanie_3 {
     public static void main(String[] args) {
         int bok = 7;
-
-
-
+double P = ((bok*bok)*Math.sqrt(3))/4;
+        System.out.println("Pole2: "+ P);
     }
 }
