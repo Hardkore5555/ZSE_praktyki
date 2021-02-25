453de86efd820b4143617ea4cef6ab6943253d1c
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-25 12:51:06 +0100

    dodano zadeklarowane zmienne do psvm

--- src/pierwszy_tydzien/zadaniadomowe/zadanie_1.java
+++ src/pierwszy_tydzien/zadaniadomowe/zadanie_1.java
@@ -1,9 +1,12 @@
 package pierwszy_tydzien.zadaniadomowe;
 
 public class zadanie_1 {
-int A = 1;
-double B = 2.22;
-char C = 'C';
-String D = "D";
+    public static void main(String[] args) {
+        int A = 1;
+        double B = 2.22;
+        char C = 'C';
+        String D = "D";
+        System.out
+    }
 
 }
