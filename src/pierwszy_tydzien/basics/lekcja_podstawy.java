9f43635b2417357e98e3af5cc07f5532d7aab55e
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-25 12:15:02 +0100

    dodano komentarz dla zmiennych

--- src/pierwszy_tydzien/basics/lekcja_podstawy.java
+++ src/pierwszy_tydzien/basics/lekcja_podstawy.java
@@ -3,7 +3,7 @@ package pierwszy_tydzien.basics;
 public class lekcja_podstawy {
 // to jest metoda główna
     public static void main(String[] args) {
-
+// liczby całkowite
         int A = 1;
         int B = -32;
         int C =  77;
