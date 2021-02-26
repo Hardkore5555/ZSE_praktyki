92f7a61aac86e555efbd8f8d7b59c21e42561c54
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-26 17:10:25 +0100

    zakończono zdanie_1

--- src/pierwszy_tydzien/zadaniadomowe2/zadanie_1.java
+++ src/pierwszy_tydzien/zadaniadomowe2/zadanie_1.java
@@ -1,10 +1,23 @@
 package pierwszy_tydzien.zadaniadomowe2;
 
+import java.util.Scanner;
+
 public class zadanie_1 {
     public static void main(String[] args) {
-        
+        Scanner podaj = new Scanner(System.in);
+
+        System.out.println("Podaj liczbę: ");
+        int liczba = podaj.nextInt();
+        if (liczba == 0) {
+            System.out.println("Podana liczba jest równa 0.");
+        } else {
+            if (liczba > 0) {
+                System.out.println("Podana liczba jest większa od 0.");
+            } else {
+                if (liczba < 0) {
+                    System.out.println("Podana liczba jest mniejsza od 0.");
+                }
+            }
+        }
     }
-
-
-
 }
