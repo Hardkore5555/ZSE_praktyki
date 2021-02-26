5b3d2cc516913455c90993307ea98d1221e78ede
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-26 17:17:39 +0100

    podawanie liczb

--- src/pierwszy_tydzien/zadaniadomowe2/zadanie_2.java
+++ src/pierwszy_tydzien/zadaniadomowe2/zadanie_2.java
@@ -1,10 +1,18 @@
 package pierwszy_tydzien.zadaniadomowe2;
 
+import java.util.Scanner;
+
 public class zadanie_2 {
     public static void main(String[] args) {
-
-
-
+        Scanner x = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int raz = x.nextInt();
+        Scanner y = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int dwa = y.nextInt();
+        Scanner z = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int trzy = z.nextInt();
 
 
 
