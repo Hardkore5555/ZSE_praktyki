9f8090553e26fd43aa4f5c80e38c19bfeaad4cfa
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-26 12:13:25 +0100

    is i else

--- src/pierwszy_tydzien/basics/if_else_scanner_switch.java
+++ src/pierwszy_tydzien/basics/if_else_scanner_switch.java
@@ -2,10 +2,19 @@ package pierwszy_tydzien.basics;
 
 public class if_else_scanner_switch {
     public static void main(String[] args) {
-        
+boolean stan = true;
+if (stan){
+    System.out.println("Wykonała się instrukacja IF");
+}
+else{
+    System.out.println("Wykonała się instrukcja else");
+}
+
+
+
+
+
+
+
     }
-
-
-
-
 }
