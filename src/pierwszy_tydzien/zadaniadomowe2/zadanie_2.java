4abb2a79b69b33f2613af61425a6ccdc0c558f81
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-26 17:45:32 +0100

    warunki równe

--- src/pierwszy_tydzien/zadaniadomowe2/zadanie_2.java
+++ src/pierwszy_tydzien/zadaniadomowe2/zadanie_2.java
@@ -13,12 +13,17 @@ public class zadanie_2 {
         Scanner z = new Scanner(System.in);
         System.out.println("Podaj liczbę: ");
         int trzy = z.nextInt();
+        if (raz == dwa) {
+            System.out.println("Pierwsza liczba jest równa drugiej.");
+        }
+        if (dwa == trzy) {
+            System.out.println("Druga liczba jest równa trzeciej.");
+        }
+        if (raz == trzy){
+            System.out.println("Pierwsza liczba jest równa trzeciej.");
+        }
+        if (){
 
-
-
-
-
-
-
+        }
     }
 }
