3c97019e869c399082420e20304e915dadf3c71e
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-26 12:17:43 +0100

    dodano skaner

--- src/pierwszy_tydzien/basics/if_else_scanner_switch.java
+++ src/pierwszy_tydzien/basics/if_else_scanner_switch.java
@@ -1,5 +1,7 @@
 package pierwszy_tydzien.basics;
 
+import java.util.Scanner;
+
 public class if_else_scanner_switch {
     public static void main(String[] args) {
 boolean stan = true;
@@ -10,9 +12,15 @@ else{
     System.out.println("Wykonała się instrukcja else");
 }
 
-
-
-
+        Scanner skaner = new Scanner(System.in);
+System.out.println("Podaj swój wiek: ");
+int wiek = skaner.nextInt();
+if (wiek >= 18) {
+    System.out.println("Jestes pełnoletni");
+}
+else{
+    System.out.println("Jestes niepełnoletni");
+    }
 
 
 
