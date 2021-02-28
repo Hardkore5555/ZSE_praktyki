18b5dee9c67f2d36d10702282e0dfd81a6e1a221
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-28 10:34:06 +0100

    zaczęto zadanie 3

--- src/pierwszy_tydzien/zadaniadomowe2/zadanie_3.java
+++ src/pierwszy_tydzien/zadaniadomowe2/zadanie_3.java
@@ -1,7 +1,29 @@
 package pierwszy_tydzien.zadaniadomowe2;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class zadanie_3 {
     public static void main(String[] args) {
-        
+        // poprawny login: admin
+        // poprawne hasło: haslo
+        Scanner login = new Scanner(System.in);
+        System.out.println("Podaj swój login: ");
+        String log = login.toString();
+        Scanner haslo = new Scanner(System.in);
+        System.out.println("Podaj swoje haslo: ");
+        String has = haslo.toString();
+        if (log == "admin") {
+
+        }
+        else{
+            System.out.println("Niepoprawny login.");
+        }
+        if (has == "haslo") {
+
+        }
+        else{
+            System.out.println("Niepoprawne hasło.");
+        }
     }
 }
