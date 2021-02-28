ca2b2b2e8bce98f0a7f69732bec7d4c9eddc5e4e
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-02-28 20:35:14 +0100

    ukończono zadanie 4

--- src/pierwszy_tydzien/zadaniadomowe2/zadanie_4.java
+++ src/pierwszy_tydzien/zadaniadomowe2/zadanie_4.java
@@ -1,16 +1,28 @@
 package pierwszy_tydzien.zadaniadomowe2;
 
+import java.util.Scanner;
+
 public class zadanie_4 {
     public static void main(String[] args) {
-
-
-
-
-
-
-
-
-
-        
+        Scanner x = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int raz = x.nextInt();
+        Scanner y = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int dwa = y.nextInt();
+        Scanner z = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int trzy = z.nextInt();
+        if (raz < dwa && dwa < trzy) {
+            System.out.println("kolejność rosnąca.");
+        } else {
+            if (trzy < dwa && dwa < raz) {
+                System.out.println("kolejność malejąca.");
+            }
+            else {
+                System.out.println("brak kolejności.");
+            }
+        }
     }
 }
+
