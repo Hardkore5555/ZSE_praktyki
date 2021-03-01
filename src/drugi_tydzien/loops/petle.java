b7e663e06ec0afa8caf417b72f075bdfc88571af
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-01 12:27:14 +0100

    pierwsza petla

--- src/drugi_tydzien/loops/petle.java
+++ src/drugi_tydzien/loops/petle.java
@@ -3,5 +3,12 @@ package drugi_tydzien.loops;
 public class petle {
     public static void main(String[] args) {
 
+        int i;
+
+        System.out.println("Pierwsza petla");
+
+        for (i=0;i<=10;i++){
+            System.out.println("i to ="+ i);
+        }
     }
 }
