0c3b7edd0c1c0fe4dcd63c638cc42e882b8c6605
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-01 12:36:42 +0100

    druga petla

--- src/drugi_tydzien/loops/petle.java
+++ src/drugi_tydzien/loops/petle.java
@@ -10,5 +10,14 @@ public class petle {
         for (i=0;i<=10;i++){
             System.out.println("i to ="+ i);
         }
+        System.out.println("Druga petla");
+        for ( int number1 = 0 , number2 = 9 ; number1 <= 9; number1++,number2-- ){
+            System.out.println("NUMBER1 = "+ number1 + " " + "NUMBER2 = "+ number2);
+        }
+
+
+
+
+
     }
 }
