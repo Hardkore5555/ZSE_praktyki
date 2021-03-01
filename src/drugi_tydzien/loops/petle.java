f6c7b7237ee1cb5b0eb02f264fe01998bd2a4158
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-01 12:48:27 +0100

    czwarta petla

--- src/drugi_tydzien/loops/petle.java
+++ src/drugi_tydzien/loops/petle.java
@@ -7,21 +7,28 @@ public class petle {
 
         System.out.println("Pierwsza petla");
 
-        for (i=0;i<=10;i++){
-            System.out.println("i to ="+ i);
+        for (i = 0; i <= 10; i++) {
+            System.out.println("i to =" + i);
         }
         System.out.println("Druga petla");
-        for ( int number1 = 0 , number2 = 9 ; number1 <= 9; number1++,number2-- ){
-            System.out.println("NUMBER1 = "+ number1 + " " + "NUMBER2 = "+ number2);
+        for (int number1 = 0, number2 = 9; number1 <= 9; number1++, number2--) {
+            System.out.println("NUMBER1 = " + number1 + " " + "NUMBER2 = " + number2);
         }
         System.out.println("Trzecia petla");
-        int x=0;
-while (x!=10){
-    x++;
-    System.out.println(x);
-}
+        int x = 0;
+        while (x != 10) {
+            x++;
+            System.out.println(x);
+        }
+        System.out.println("Czwarta petla");
+        int numberC = 6;
+        do {
 
+            System.out.println("Zmienna =" + numberC);
+            numberC++;
+        } while (numberC < 12);
 
 
     }
 }
+
