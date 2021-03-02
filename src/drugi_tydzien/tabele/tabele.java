5843a49474be2600beb32fa5d8c1a96c5a9232a4
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-02 12:15:01 +0100

    tablice 1

--- src/drugi_tydzien/tabele/tabele.java
+++ src/drugi_tydzien/tabele/tabele.java
@@ -1,7 +1,23 @@
 package drugi_tydzien.tabele;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class tabele {
     public static void main(String[] args) {
-        
+        int[] array = {1,45,56,-23};
+        int array2[] = {-7,6,52,2,100,0,777};
+
+        System.out.println("Element pierwszy tablicy array[0]: "+array[0]);
+        System.out.println("Długość tablicy array: "+array.length);
+        System.out.println("Element pierwszy tablicy array2[0]: "+array2[0]);
+        System.out.println("Długość tablicy array2: "+array2.length);
+
+        System.out.println("\n\nTablica array przed zmianą: "+ Arrays.toString(array));
+
+        array[0]=15;
+        array[1]=10;
+        array[2]=5;
+        array[3]=0;
     }
 }
