1a9e70c9ac8801cc3d96599e8887a8ab61a27b18
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-03 22:37:24 +0100

    ukończono zadanie 4

--- src/drugi_tydzien/zadaniadomowe1/zadanie_4.java
+++ src/drugi_tydzien/zadaniadomowe1/zadanie_4.java
@@ -2,10 +2,11 @@ package drugi_tydzien.zadaniadomowe1;
 
 public class zadanie_4 {
     public static void main(String[] args) {
-
-
-
-
-        
+        int i;
+        for (i = -40; i <= 40; i++) {
+            if (i%4==0) {
+                System.out.println("i to =" + i);
+            }
+        }
     }
 }
