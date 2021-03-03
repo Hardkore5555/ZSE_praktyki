4b56b2dc8134377489d8bddd9933d30e871808cf
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-03 12:08:02 +0100

    zadanie 3 petle

--- src/drugi_tydzien/zadaniadomowe1/zadanie_3.java
+++ src/drugi_tydzien/zadaniadomowe1/zadanie_3.java
@@ -2,6 +2,15 @@ package drugi_tydzien.zadaniadomowe1;
 
 public class zadanie_3 {
     public static void main(String[] args) {
-
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i + " przejscie petli");
+            for (int j = 1; j < 11; j++) {
+                if (i / 2 == 0) {
+                    System.out.println("X\n");
+                } else {
+                    System.out.println(" [l:" + i + "]" + "[j:" + i + "]\t " + i + " * " + i + " = " + i * j + "\t");
+                }
+            }
+        }
     }
 }
