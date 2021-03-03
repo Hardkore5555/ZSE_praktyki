b1c5678bc8b7016c48c1ffbc3f8d57901e518644
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-03 22:55:39 +0100

    zakończono zadanie 5

--- src/drugi_tydzien/zadaniadomowe1/zadanie_5.java
+++ src/drugi_tydzien/zadaniadomowe1/zadanie_5.java
@@ -2,12 +2,18 @@ package drugi_tydzien.zadaniadomowe1;
 
 public class zadanie_5 {
     public static void main(String[] args) {
-
-
-
-
-
-
-        
+//przerobiony kod z zadania 1
+        int i = 0;
+        while (i < 20) {
+            i += 2;
+            System.out.println("i to =" + i);
+        }
+//przerobiony kod z zadania 2
+        System.out.println("Pętla 2\n");
+        int j = 30;
+        do {
+            System.out.println("j to =" + j);
+            j-=3;
+        } while (j >= 0);
     }
 }
