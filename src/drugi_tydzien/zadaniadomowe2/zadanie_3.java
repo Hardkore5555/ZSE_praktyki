495d383da63b0532e624c90cfc26fb97008f8bf9
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-04 12:02:05 +0100

    zakończono zadanie 3

--- src/drugi_tydzien/zadaniadomowe2/zadanie_3.java
+++ src/drugi_tydzien/zadaniadomowe2/zadanie_3.java
@@ -1,12 +1,26 @@
 package drugi_tydzien.zadaniadomowe2;
 
+import java.util.Random;
+
 public class zadanie_3 {
     public static void main(String[] args) {
 
+        Random random = new Random();
 
 
+        String[] ImionaPsa = {"Hektor", "Aleks", "Maki"};
+        System.out.println("");
+        int LosoweImiePsa = random.nextInt(3) + 1;
+        if (LosoweImiePsa == 1) {
+            System.out.println("Wylosowane imię dla psa: " + ImionaPsa[0]);
+        }
+        if (LosoweImiePsa == 2) {
+            System.out.println("Wylosowane imię dla psa: " + ImionaPsa[1]);
+        }
+        if (LosoweImiePsa == 3) {
+            System.out.println("Wylosowane imię dla psa: " + ImionaPsa[2]);
+        }
 
-
-        
     }
 }
+
