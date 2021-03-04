58f71d782c5a59d6d9cebd97928098123044690d
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-04 11:46:53 +0100

    zakończono zadanie 1 i zadanie 2

--- src/drugi_tydzien/zadaniadomowe2/zadanie_1.java
+++ src/drugi_tydzien/zadaniadomowe2/zadanie_1.java
@@ -1,11 +1,12 @@
 package drugi_tydzien.zadaniadomowe2;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class zadanie_1 {
     public static void main(String[] args) {
-
-
-
-        
-
+        char[] charArray = {'z','w','j','e','g','s','p','q','m','a'};
+        Arrays.sort(charArray);
+        System.out.println("Sortowanie w porządku rosnącym: " + Arrays.toString(charArray));
     }
 }
--- src/drugi_tydzien/zadaniadomowe2/zadanie_2.java
+++ src/drugi_tydzien/zadaniadomowe2/zadanie_2.java
@@ -1,15 +1,12 @@
 package drugi_tydzien.zadaniadomowe2;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class zadanie_2 {
     public static void main(String[] args) {
-
-
-
-
-
-
-
-
-
+        String[] stringArray = {"a","b","c","d","e","f","g","h","i","j"};
+        Arrays.sort(stringArray, Collections.reverseOrder());
+        System.out.println("Sortowanie w porządku malejącym: " + Arrays.toString(stringArray));
     }
 }
