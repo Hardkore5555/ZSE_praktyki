b7b6a043255278226fbd09bc0e775986e769753c
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-07 16:24:54 +0100

    ukończono zadanie 1

--- src/drugi_tydzien/zadaniadomowe3/zadanie_1.java
+++ src/drugi_tydzien/zadaniadomowe3/zadanie_1.java
@@ -3,8 +3,17 @@ package drugi_tydzien.zadaniadomowe3;
 public class zadanie_1 {
     public static void main(String[] args) {
 
-
-
-
+        int[][] tab = new int[5][5];
+        int value = 1;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                tab[i][j] = value;
+                value++;
+                System.out.print(tab[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
+
+
