d35035f5f52071b2ab966d089a773bd9641b420e
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-07 13:00:41 +0100

    zadanie_4

--- src/drugi_tydzien/zadaniadomowe2/zadanie_4.java
+++ src/drugi_tydzien/zadaniadomowe2/zadanie_4.java
@@ -1,7 +1,12 @@
 package drugi_tydzien.zadaniadomowe2;
 
+import java.util.Random;
+
 public class zadanie_4 {
     public static void main(String[] args) {
-        
+        String[] jedzenie = {"Placki","Frytki","Bigos","Makaron","Jajecznica","Tosty","Zupa"};
+        Random x = new Random();
+        int potrawy = x.nextInt(jedzenie.length);
+        System.out.println("Na śniadanie zjem: "+potrawy+", na obiad: "+potrawy+", a na kolację: "+potrawy);
     }
 }
