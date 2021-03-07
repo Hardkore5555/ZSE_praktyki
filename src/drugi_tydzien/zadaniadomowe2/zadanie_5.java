d9f5381fd6675825a01682b906216870b8580439
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-07 13:25:44 +0100

    ukończono zadanie 5

--- src/drugi_tydzien/zadaniadomowe2/zadanie_5.java
+++ src/drugi_tydzien/zadaniadomowe2/zadanie_5.java
@@ -1,10 +1,15 @@
 package drugi_tydzien.zadaniadomowe2;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class zadanie_5 {
     public static void main(String[] args) {
-
-
-
-        
+        Integer Array[] = {6, -4, 5, 2, 8, 10, 4, 3, 7, 9, -14, 12, -2, 1, -5, -1, 11, 18, -19, -11};
+        Arrays.sort(Array, 0, 19);
+        Arrays.sort(Array, 10, 19, Collections.reverseOrder());
+        System.out.println("Posortowana tablica:\t"+Arrays.toString(Array));
     }
 }
+
