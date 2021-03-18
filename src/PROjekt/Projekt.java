3e99ac041620bb65ca47e72dd88bba2b1d693ea1
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-18 20:59:28 +0100

    projekt

--- src/PROjekt/Projekt.java
+++ src/PROjekt/Projekt.java
@@ -7,31 +7,380 @@ public class Projekt {
 
         Scanner skaner = new Scanner(System.in);
 
-        System.out.println("Pytanie: Ile pies ma nog?" +
-                           "   a)1 | b)2 | c)3 | d)4                    ");
+        System.out.println("Pytanie 1: Ile pies ma nog?\n" +
+                "a) 1\n" +
+                "B) 2\n" +
+                "C) 3\n" +
+                "D) 4");
 
-        char odpowiedzi = skaner.next().charAt(0);
-switch (odpowiedzi) {
-    case 'a':
-        System.out.println("zła odpowiedź");
-        break;
-    case 'b':
-        System.out.println("zła odpowiedź");
-        break;
-    case 'c':
-        System.out.println("zła odpowiedź");
-        break;
-    case 'd':
-        System.out.println("dobra odpowiedź");
-        break;
-    default:
-}
-
-
-
-
-
-
+        char odpowiedzi1 = skaner.next().charAt(0);
+        switch (odpowiedzi1) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 2: Gdzie jest kassadin?\n     " +
+                "a)na topie\n" +
+                "B)na midzie\n" +
+                "C)na bocie\n" +
+                "D)w jungli");
+        char odpowiedzi2 = skaner.next().charAt(0);
+        switch (odpowiedzi2) {
+            case 'A':
+                System.out.println("dobra odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("zła odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 3: W języku staropolskim matka żony to teściowa, a matka męża to: \n" +
+                "A)teściowa\n" +
+                "B)świekra\n" +
+                "C)zełwa\n" +
+                "D)snecha      " +
+                "                 ");
+        char odpowiedzi3 = skaner.next().charAt(0);
+        switch (odpowiedzi3) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("dobra odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("zła odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 4:Jakiego koloru jest czarna skrzynka w samolocie?\n" +
+                "A)czarna\n" +
+                "B)czerwona\n" +
+                "C)pomarańczowa \n" +
+                "D)szara      " +
+                "                 ");
+        char odpowiedzi4 = skaner.next().charAt(0);
+        switch (odpowiedzi4) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("dobra odpowiedź");
+                break;
+            case 'D':
+                System.out.println("zła odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 5:      " +
+                "                 ");
+        char odpowiedzi5 = skaner.next().charAt(0);
+        switch (odpowiedzi5) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 6:      " +
+                "                 ");
+        char odpowiedzi6 = skaner.next().charAt(0);
+        switch (odpowiedzi6) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 7:      " +
+                "                 ");
+        char odpowiedzi7 = skaner.next().charAt(0);
+        switch (odpowiedzi7) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 8:      " +
+                "                 ");
+        char odpowiedzi8 = skaner.next().charAt(0);
+        switch (odpowiedzi8) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 9:      " +
+                "                 ");
+        char odpowiedzi9 = skaner.next().charAt(0);
+        switch (odpowiedzi9) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 10:      " +
+                "                 ");
+        char odpowiedzi10 = skaner.next().charAt(0);
+        switch (odpowiedzi10) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 11:      " +
+                "                 ");
+        char odpowiedzi11 = skaner.next().charAt(0);
+        switch (odpowiedzi11) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 12:      " +
+                "                 ");
+        char odpowiedzi12 = skaner.next().charAt(0);
+        switch (odpowiedzi12) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 13:      " +
+                "                 ");
+        char odpowiedzi13 = skaner.next().charAt(0);
+        switch (odpowiedzi13) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 14:      " +
+                "                 ");
+        char odpowiedzi14 = skaner.next().charAt(0);
+        switch (odpowiedzi14) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 15:      " +
+                "                 ");
+        char odpowiedzi15 = skaner.next().charAt(0);
+        switch (odpowiedzi15) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 16:      " +
+                "                 ");
+        char odpowiedzi16 = skaner.next().charAt(0);
+        switch (odpowiedzi16) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 17:      " +
+                "                 ");
+        char odpowiedzi17 = skaner.next().charAt(0);
+        switch (odpowiedzi17) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 18:      " +
+                "                 ");
+        char odpowiedzi18 = skaner.next().charAt(0);
+        switch (odpowiedzi18) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 19:      " +
+                "                 ");
+        char odpowiedzi19 = skaner.next().charAt(0);
+        switch (odpowiedzi19) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
+        System.out.println("Pytanie 20:      " +
+                "                 ");
+        char odpowiedzi20 = skaner.next().charAt(0);
+        switch (odpowiedzi20) {
+            case 'A':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'B':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'C':
+                System.out.println("zła odpowiedź");
+                break;
+            case 'D':
+                System.out.println("dobra odpowiedź");
+                break;
+            default:
+        }
     }
-
 }
