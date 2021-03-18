20b73405afd776f69102d236ce5637c19660e560
Hardkore5555 <56128365+Hardkore5555@users.noreply.github.com>
2021-03-18 20:37:22 +0100

    projekt

--- src/PROjekt/Projekt.java
+++ src/PROjekt/Projekt.java
@@ -1,9 +1,31 @@
 package PROjekt;
 
+import java.util.Scanner;
+
 public class Projekt {
     public static void main(String[] args) {
+
+        Scanner skaner = new Scanner(System.in);
+
         System.out.println("Pytanie: Ile pies ma nog?" +
                            "   a)1 | b)2 | c)3 | d)4                    ");
+
+        char odpowiedzi = skaner.next().charAt(0);
+switch (odpowiedzi) {
+    case 'a':
+        System.out.println("zła odpowiedź");
+        break;
+    case 'b':
+        System.out.println("zła odpowiedź");
+        break;
+    case 'c':
+        System.out.println("zła odpowiedź");
+        break;
+    case 'd':
+        System.out.println("dobra odpowiedź");
+        break;
+    default:
+}
 
 
 
